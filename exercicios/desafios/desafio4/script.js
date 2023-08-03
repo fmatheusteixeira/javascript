fba5496460c2ba82f8b42f7637cad4c3784ea68c
Thainara Lima <matheus_bteixeira@hotmail.com>
2023-08-02 21:31:56 -0300

    finalizando desafio 2 de tabuada com JS

--- exercicios/desafios/desafio4/script.js
+++ exercicios/desafios/desafio4/script.js
@@ -1,7 +1,19 @@
 function calculo() {
     var num = document.querySelector('input#num')
-    var res = document.querySelector('select#res')
+    var res = document.querySelector('select#seltab')
 
-    if (num.value.length == 0)
-        res.innerHTML = 'ERRO'
+    var resultado = document.createElement('p')
+    if (num.value.length == 0) {
+        alert('ERRO!')
+}
+    else {
+      var n = Number(num.value)
+        res.innerHTML = ''
+        for (var c = 1; c <= 10; c++) {
+            var opt = document.createElement('option')
+            opt.text = `${n} x ${c} = ${n*c}`
+            res.appendChild(opt)
+        }
+    }
+    
 }
