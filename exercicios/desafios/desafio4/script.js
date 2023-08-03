a937310a681c7410d5d6749028739657b5754b4c
fmatheusteixeira <matheus_bteixeira@hotmail.com>
2023-08-03 14:01:17 -0300

    exercicio de desconto JS

--- exercicios/desafios/desafio4/script.js
+++ exercicios/desafios/desafio4/script.js
@@ -2,7 +2,6 @@ function calculo() {
     var num = document.querySelector('input#num')
     var res = document.querySelector('select#seltab')
 
-    var resultado = document.createElement('p')
     if (num.value.length == 0) {
         alert('ERRO!')
 }
@@ -11,7 +10,7 @@ function calculo() {
         res.innerHTML = ''
         for (var c = 1; c <= 10; c++) {
             var opt = document.createElement('option')
-            opt.text = `${n} x ${c} = ${n*c}`
+            opt.textContent = `${n} x ${c} = ${n*c}`
             res.appendChild(opt)
         }
     }
--- exercicios/ex014.js
+++ exercicios/ex014.js
@@ -1,7 +1,9 @@
 // ESTRUTURA DE REPETIÇÃO COM VARIAVEL DE CONTROLE
 
 var n = 1
+
 for (var n=1; n <= 10; n++) {
     console.log(`Comer ${n} fatia`)
+
 }
 console.log('Acabou')
