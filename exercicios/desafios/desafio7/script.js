4972530e3712ff5fdc7799ee705a5d987a783e46
fmatheusteixeira <matheus_bteixeira@hotmail.com>
2023-08-07 16:41:24 -0300

    exercicio de função JS

---
+++ exercicios/desafios/desafio7/script.js
@@ -0,0 +1,17 @@
+var num = Number(document.getElementById('txtn').value)
+var mostrador = document.getElementById('selnum')
+var res = document.getElementById('res')
+var valores = []
+
+function adicionar() {
+
+    if (num < 1 || num > 100) {
+        
+        alert('ERRO! Verifique se o número está entre 1 e 100')
+    }
+    
+    else {
+           
+    }
+
+}
